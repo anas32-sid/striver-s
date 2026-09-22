31b96cb05a26feb50d5539adacc45781f72503b5
mohd. anas <anas9650731343@gmail.com>
2026-09-22 12:30:33 +0530

    Update reverse_a_string.cpp

--- reverse_a_string.cpp
+++ reverse_a_string.cpp
@@ -11,3 +11,83 @@ public:
         }
     }
 };
+
+/*
+Dry Run
+
+let array: s = ['c', 'o', 'd', 'e']`
+
+Initially:
+
+`left = 0`
+`right = 3`
+
+So:
+
+* `s[0] = 'c'`
+* `s[3] = 'e'`
+
+Now we check the condition:
+
+left<right → 0<3 → true
+
+So, the loop executes and we swap `s[left]` and `s[right]`.
+
+After swapping:
+
+`s = ['e', 'o', 'd', 'c']`
+
+Then:
+
+`left++` → `left = 1`
+`right--` → `right = 2`
+
+---
+
+### Second Iteration
+
+Now:
+
+`left = 1`
+`right = 2`
+
+So:
+
+* `s[1] = 'o'`
+* `s[2] = 'd'`
+
+Check the condition:
+
+`left < right` → `1 < 2` → **true**
+
+The loop executes again, and we swap `s[left]` and `s[right]`.
+
+After swapping:
+
+`s = ['e', 'd', 'o', 'c']`
+
+Then:
+
+`left++` → `left = 2`
+`right--` → `right = 1`
+
+---
+
+### Final Check
+
+Now:
+
+`left = 2`
+`right = 1`
+
+Check the condition:
+
+`left<right` → `2 < 1` → **false**
+
+Therefore, the `while` loop stops, and the code cannot execute another iteration.
+
+Final reversed array:
+
+s = ['e', 'd', 'o', 'c']
+
+*/
