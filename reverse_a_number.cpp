235c16a59ae91a797d22148007127e91492ca85a
mohd. anas <anas9650731343@gmail.com>
2026-09-22 14:30:46 +0530

    Update reverse_a_number.cpp

--- reverse_a_number.cpp
+++ reverse_a_number.cpp
@@ -1,18 +1,80 @@
-#include <iostream>
-using namespace std;
-int reverse_number(int n){
-    int lastdigit=0;
-    int reverse=0;
-    while(n>0){
-        lastdigit=n%10;
-        reverse=(reverse*10)+lastdigit;
-        n=n/10;
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+        int temp = x;
+        int lastdigit = 0;
+        long long reverse = 0;
+
+        while (x > 0) {
+            lastdigit = x % 10;
+            reverse = (reverse * 10) + lastdigit;
+            x = x / 10;
+        }
+        return temp == reverse;
     }
-    return reverse;
-}
-int main(){
-int n;
-cout<<"enter n: ";
-cin>>n;
-cout<<"revse n is: "<<reverse_number(n);
-}
+};
+/*
+Dry Run
+let number: x = 121
+Initially:
+x = 121
+temp = 121
+lastdigit = 0
+reverse = 0
+
+First check:
+x<0 (121<0) → false (skip if-block)
+
+Now we check the condition:
+
+(x>0) → 121>0 =true
+
+So, the loop will executes.
+
+lastdigit = 121 % 10 = 1
+reverse = (0*10) + 1 = 1
+x = 121/10 = 12
+
+---
+
+Iteration 2
+x = 12
+
+Check the condition:
+
+(x>0) (12>0) = true
+
+lastdigit = 12 % 10 → 2
+reverse = (1 * 10) + 2 → 12
+x = 12/10 = 1
+
+---
+
+Iteration 3
+x = 1
+
+Check the condition:
+
+(x>0) → (1>0) → true
+
+lastdigit = 1 % 10` → `1`
+reverse = (12 * 10) + 1` → `121`
+x = 1/10 = 0
+
+### Final Check
+
+Now:
+x = 0
+Check the condition:
+
+x>0 → (0>0) = false
+
+Therefore, the `while` loop stops, and the code cannot execute another iteration.
+
+Final check for return:
+temp == reverse → 121 == 121 → true
+Returns true.
+*/
